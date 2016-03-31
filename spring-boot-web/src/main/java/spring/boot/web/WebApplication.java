9408153bc0e784ccb3058c2b7ff548d95dda343e
ChoYongSang <chotire@gmail.com>
2016-03-31 13:34:46 +0900

    Spring Boot Actuator 예제 추가

--- spring-boot-web/src/main/java/spring/boot/web/WebApplication.java
+++ spring-boot-web/src/main/java/spring/boot/web/WebApplication.java
@@ -8,11 +8,11 @@ import spring.boot.web.config.MybatisConfiguration;
 
 @SpringBootApplication
 @Import(MybatisConfiguration.class)
-public class SpringBootWebApplication {
+public class WebApplication {
     public static void main(String[] args) {
 //      SpringApplication app = new SpringApplication(SpringBootWebApplication.class);
 //      app.setBannerMode(Mode.OFF);
 //      app.run(args);
-        SpringApplication.run(SpringBootWebApplication.class, args);
+        SpringApplication.run(WebApplication.class, args);
     }
 }
